3af80f2f1ca6bdc3d5092cd161ff57e37cc744ac
Jahid Hasan <jh3495689@gmail.com>
2023-05-27 11:38:41 +0600

    chefserves section add

---
+++ src/Pages/Home/ChefServes/ChefServes.jsx
@@ -0,0 +1,14 @@
+import './ChefServes.css'
+
+const ChefServes = () => {
+    return (
+        <div className='chef-serves my-28'>
+            <div className='text-center relative m-16 bg-white py-10 top-24'>
+                <h1 className='text-3xl'>BISTRO BOSS</h1>
+                <p className='p-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, sunt ut praesentium quidem laboriosam minus. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis, ullam.</p>
+            </div>
+        </div>        
+    );
+};
+
+export default ChefServes;
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,5 +1,6 @@
 import Banner from "../Banner/Banner";
 import Category from "../Category/Category";
+import ChefServes from "../ChefServes/ChefServes";
 
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
         <div>
             <Banner></Banner>
             <Category></Category>
+            <ChefServes></ChefServes>
         </div>
     );
 };
